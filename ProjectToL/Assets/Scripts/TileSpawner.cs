46cf45591a52e52394e81548066783fc29a2619c
jyjiwoo <thswldn0317@kau.kr>
2024-03-17 21:40:29 +0900

    Map making
    240317 jiwoo

---
+++ ProjectToL/Assets/Scripts/TileSpawner.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSpawner : MonoBehaviour
+{
+    [Header("Board Info")]
+    [SerializeField]
+    private int boardWidth;
+    [SerializeField]
+    private int boardHeight;
+
+    [SerializeField]
+    private GameObject tilePrefab;
+    [SerializeField]
+    private Transform parentTransform;
+
+    private int balanceSize = 5;
+    private float spacing = 0.1f;
+
+
+
+    private void Awake()
+    {
+        for (int y = 0; y < boardHeight; y++)
+        {
+            for (int x = 0; x < boardWidth; x++)
+            {
+                GameObject clone = Instantiate(tilePrefab, new Vector3((x + spacing * x) * balanceSize, 0, (y + spacing * y) * balanceSize), Quaternion.identity, parentTransform);
+            }
+        }
+    }
+}
