35c1a411a7043b868d80c8eb69c531caef02b20a
Medorpond <stcwh@naver.com>
2024-03-23 16:09:55 +0900

    Script Reform
     - TileHandler Removed
     - GameManager Script Added.

--- ProjectToL/Assets/Scripts/GameManager.cs
+++ ProjectToL/Assets/Scripts/GameManager.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TileHandler : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
+    // ScreenPointRay 기능을 활용해 화면에서 마우스를 사용한 상호작용을 가능케 함.
+
     // Start is called before the first frame update
     void Start()
     {
